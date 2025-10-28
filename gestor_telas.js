4d30d3b7753e4dc1905a5cf332db77c56a7187cb
CESAR_C# <lianbastiam@gmail.com>
2025-10-28 16:39:32 -0300

    telas consertadas

--- gestor_telas.js
+++ gestor_telas.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import transition from './screen/transactions';
+import login from './screen/login';
+import home from './screen/home';
+
 export default function App() {
   return (
     <View style={styles.container}>
