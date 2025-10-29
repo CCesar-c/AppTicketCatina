965470695e4762acf58b75dcb10e78688349c302
CESAR_C# <lianbastiam@gmail.com>
2025-10-29 15:42:25 -0300

    fixed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './screen/login';
+import Login from './screens/login';
 import { useEffect } from 'react';
 
 export default function App() {
