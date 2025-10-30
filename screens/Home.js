07570e87940e6e3a16d37ebeefb9a8ba7f9806e3
GustavoCamargo0 <gustavo.fscamargo08@gmail.com>
2025-10-30 14:43:16 -0300

    fixed

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 24, fontWeight: 'bold', margin: 10 }}>Saldo de tickets: </Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
