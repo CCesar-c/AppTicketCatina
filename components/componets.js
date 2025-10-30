328b71341dbc4d6adc34762778d305ef50c1ddeb
CESAR_C# <lianbastiam@gmail.com>
2025-10-30 16:03:50 -0300

    add style button new

--- components/componets.js
+++ components/componets.js
@@ -1,9 +1,9 @@
 import { TouchableOpacity, Text, View } from "react-native";
 
-export function NewButton({style,children, onPress }) {
+export function NewButton({style, children, onPress, ...props }) {
     return (
         <View>
-            <TouchableOpacity style={style || { borderWidth:1, borderColor:"black" }} onPress={onPress} >
+            <TouchableOpacity style={style || { borderWidth:1, borderColor:"black" }} onPress={onPress} {...props} >
                 <Text>
                     {children}
                 </Text>
--- screens/Home.js
+++ screens/Home.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {NewButton } from '../components/componets'
-
+import { NewButton } from '../components/componets'
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 24, fontWeight: 'bold', margin: 10 }}>Saldo de tickets: </Text>
-      <NewButton onPress={() =>{alert("querro goza")}} > 
+      <NewButton onPress={() => { alert("querro goza") }} activeOpacity={0.6}>
         Carregar creditos
+      </NewButton>
+      <NewButton  onPress={() => { alert("querro goza") }} activeOpacity={0.6} >
+        Comprar na cantina
       </NewButton>
     </View>
   );
