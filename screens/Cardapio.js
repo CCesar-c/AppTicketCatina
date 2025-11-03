4dd88fcd768ada83581d8982e90c9be899fdd037
CESAR_C# <lianbastiam@gmail.com>
2025-11-03 17:00:43 -0300

    add random

--- screens/Cardapio.js
+++ screens/Cardapio.js
@@ -11,61 +11,61 @@ export default function Cardapio({ navigation }) {
   const [result, setResult] = useState([]);
 
   useEffect(() => {
-    // const fetchFotos = async () => {
-    //   const res = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b');
-    //   const json = await res.json();
-    //   setResult(json.meals || []);
-    //   // for (let index = 0; index < 30; index++) {
-    //   //   const res = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
-    //   //   const json = await res.json();
-    //   //   setResult((prevFotos) => {
-    //   //     return [...prevFotos, ...(json.meals || [])]
-    //   //   });
-    //   // }
+    const fetchFotos = async () => {
+      // https://www.themealdb.com/api/json/v1/1/random.php
+      // const res = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b');
+      // const json = await res.json();
+      // setResult(json || []);
+      for (let index = 0; index < 20; index++) {
+        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
+        const json = await res.json();
+        setResult((prevFotos) => {
+          return [...prevFotos, ...(json.meals || [])]
+        });
+      }
 
-    // };
-    // fetchFotos();
-    // console.log
+    };
+    fetchFotos();
 
-    const fetchGeneral = async () => {
-      const { data: comidas } = await supabase
-        .from('Comidas')
-        .select('*');
-      const { data: bebidas } = await supabase
-        .from('Bebidas')
-        .select('*');
-      const { data: outros } = await supabase
-        .from('Outras opcoes')
-        .select('*');
+    // const fetchGeneral = async () => {
+    //   const { data: comidas } = await supabase
+    //     .from('Comidas')
+    //     .select('*');
+    //   const { data: bebidas } = await supabase
+    //     .from('Bebidas')
+    //     .select('*');
+    //   const { data: outros } = await supabase
+    //     .from('Outras opcoes')
+    //     .select('*');
 
-      setResult([...(comidas || []), ...(bebidas || []), ...(outros || [])]);
-    }
-    fetchGeneral();
-  }, []);
+    //   setResult([...(comidas || []), ...(bebidas || []), ...(outros || [])]);
+    // }
+    // fetchGeneral();
+  },  1);
 
   return (
-    <View style={[{ height: '30%', backgroundColor: theme.background }]}>
+    <View style={[{ height: '85%', backgroundColor: theme.background }]}>
       <ScrollView showsVerticalScrollIndicator={true} contentContainerStyle={styles.container}>
-        {result.map((item) => {
+        {result.map((item, index) => {
           return (
-            <View key={item.id} style={styles.card}>
+            <View key={index} style={styles.card}>
               <Image
-                // source={item}
+                source={{uri: item.strMealThumb }}
                 style={styles.image}
                 resizeMode="cover"
               />
               <Text style={styles.text}>
-                🍽️ Nome: {item.Nome}{"\n"}
-                💰 Preço: {item.Valor + " contos"}
+                🍽️ Nome: {item.strMeal}{"\n"}
+                {/* 💰 Preço: {item.Valor + " contos"} */}
               </Text>
               <NewButton
                 style={{ width: '120px', height: '60px', backgroundColor: '#28a745', borderRadius: 5, marginTop: 10, }}
                 onPress={async () => {
                   navigation.navigate('DetalhesCompras', {
-                    nombre: item.Nome,
-                    valor: item.Valor,
-                    fotoproduto: foto,
-                  }); alert(`Adicionado ${item.Nome} ao carrinho!`); await AsyncStorage.setItem("carrinho", item.Nome)
+                    nombre: item.strMeal,
+                    // valor: item.Valor,
+                    fotoproduto: item.strMealThumb ,
+                  }); alert(`Adicionado ${item.strMeal} ao carrinho!`); await AsyncStorage.setItem("carrinho",item.strMeal)
                 }}>
                 {"Adicionar ao Carrinho"}
               </NewButton>
