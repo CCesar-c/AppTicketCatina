f0e358b159a7b21489f471273aeb5181e9cc793f
Davi Grah <davigrah2010@gmail.com>
2025-11-04 15:49:07 -0300

    .

--- screens/DetalhesCompras.js
+++ screens/DetalhesCompras.js
@@ -1,29 +1,8 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 //import { useNavigation } from "@react-navigation/native";
 import { useRoute } from "@react-navigation/native";
-import { supabase } from '../Back-end/supabase';
 export default function DetalhesCompras() {
-    const [fotoUrl, setFotoUrl] = useState("");
-    useEffect(() => {
-        const buscarFoto = async () => {
-          
-          const url = "https://meuservidor.com/imagens/aluno1.jpg";
-          setFotoUrl(url);
-        };
-        buscarFoto();
-      }, []);
-    
-    const result = useRoute();
-    const { fotoproduto, nombre, valor } = result.params;
-    
-        const fetchGeneral = async () => {
-          const { data: img } = await supabase
-            .storage
-            .from("Imagens")
-            .list('*')
-        }
-        fetchGeneral();
-    
+   
     return (
         <View style={styles.container}>
             <Image
@@ -32,7 +11,7 @@ export default function DetalhesCompras() {
                 resizeMode="contain"
             />
             <Text style={styles.text}>{nombre}</Text>
-            <Text style={styles.text}>💰 {valor} contos</Text>
+            <Text style={styles.text}>💰 {valor}$</Text>
         </View>
     )
 }
