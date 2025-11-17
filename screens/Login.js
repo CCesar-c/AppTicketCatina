8dd12a220aac2304a825117219c96ba03f7988ac
CESAR_C# <lianbastiam@gmail.com>
2025-11-17 16:17:55 -0300

    fixed

--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,18 @@
-import { useContext, useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, Alert, ImageBackground, Image } from 'react-native';
+import { useContext, useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TextInput, Alert, Image } from 'react-native';
 import { supabase } from '../Back-end/supabase';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NewButton from '../components/componets';
 import { ThemeContext } from '../contexts/themeContext';
-import Logo from '../assets/LogoCantina.png'
+import * as Animatable from 'react-native-animatable';
+import Logo from '../assets/Cantina_Tickect_ja.png'
 
 export default function Login({ navigation }) {
   const { theme } = useContext(ThemeContext);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
-
+  const ref = useRef('')
   useEffect(() => {
     async function loadData() {
       const storedName = await AsyncStorage.getItem('@storage_Name');
@@ -26,16 +27,13 @@ export default function Login({ navigation }) {
 
   async function loadUsers() {
     if (!name || !email || !pass) {
-      Alert.alert('Erro', 'Por favor, preencha todos os campos.');
-      return;
+      ref.current.shake(200)
     }
-
     const { data, error } = await supabase
       .from('users')
       .select('*')
       .eq('Emails', email)
       .eq('Senha', pass);
-
     if (error) {
       console.log('❌ Error:', error.message);
       alert('Erro', 'Erro ao buscar usuário.');
@@ -47,51 +45,59 @@ export default function Login({ navigation }) {
       return;
     }
 
+
     if (data[0].Administrador === true) {
       navigation.navigate('RouterAdmin');
       alert('Acesso concedido ' + 'Bem-vindo administrador!');
+      ref.current.fadeOut(200)
     } else if (data[0].Administrador === false) {
       navigation.navigate('Drawer');
       alert('Acesso concedido ' + 'Bem-vindo usuário!');
+      ref.current.fadeOut(200)
     } else {
       console.error("Erro de autorizaçao")
+      ref.current.shake(200)
     }
-
     storeData();
   }
 
   return (
     <View style={[styles.container, { backgroundColor: theme.background }]}>
-      <ImageBackground
-        source={Logo}
-        style={{ width: 150, height: 200 }}>
-      </ImageBackground>
-      <Text style={[styles.title, { color: theme.text }]}>Login</Text>
+      <Animatable.View animation={'fadeInUp'} ref={ref} style={[styles.container, { backgroundColor: theme.background }]}>
+        <Image
+          source={Logo}
+          style={{ width: 150, height: 200 }}>
+        </Image>
+        <Text style={[styles.title, { color: theme.text }]}>Login</Text>
 
-      <TextInput
-        style={[styles.input, { color: theme.text }]}
-        placeholder="Digite Seu Nome"
-        value={name}
-        onChangeText={setName}
-        keyboardType="default"
-      />
-      <TextInput
-        style={[styles.input, { color: theme.text }]}
-        placeholder="Digite Seu Email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={[styles.input, { color: theme.text }]}
-        placeholder="Digite Sua Senha"
-        value={pass}
-        onChangeText={setPass}
-        secureTextEntry
-        keyboardType="default"
-      />
+        <TextInput
+          style={[styles.input, { color: theme.text }]}
+          placeholder="Digite Seu Nome"
+          value={name}
+          onChangeText={setName}
+          keyboardType="default"
+        />
+        <TextInput
 
-      <NewButton onPress={loadUsers}>Entrar</NewButton>
+          style={[styles.input, { color: theme.text }]}
+          placeholder="Digite Seu Email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={[styles.input, { color: theme.text }]}
+          placeholder="Digite Sua Senha"
+          value={pass}
+          onChangeText={setPass}
+          secureTextEntry
+          keyboardType="default"
+        />
+
+        <NewButton onPress={() => {
+          loadUsers()
+        }}>Entrar</NewButton>
+      </Animatable.View>
     </View>
   );
 }
